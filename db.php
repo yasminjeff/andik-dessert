e2a9528dcce771c020a243afed143685349d9d18
melianamarissa@gmail.com <melianamarissa@gmail.com>
2026-05-31 11:49:41 +0800

    Set the Railway credentials inside db.php

--- db.php
+++ db.php
@@ -4,12 +4,13 @@ class DB {
         if ($_SERVER['HTTP_HOST'] === 'localhost' || str_starts_with($_SERVER['HTTP_HOST'], 'localhost:')) {
             $db = new PDO("mysql:host=localhost;dbname=andiks_desserts;charset=utf8mb4", "root", "");
         } else {
-            $host = 'sql306.infinityfree.com';
-            $dbname = 'if0_41989340_andiks_desserts';
-            $username = 'if0_41989340';
-            $password = 'jgwbK3TOo7xn';
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-        }
+    $host = 'zephyr.proxy.rlwy.net';
+    $dbname = 'railway';
+    $username = 'root';
+    $password = 'OdiCtRykjBAYZuuOQDmppeALkvipdYaO';
+    $port = '45879';
+    $db = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
+}
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     }
