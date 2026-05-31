07c8ed9431d32d193f2b065d3e5b8ef57770daec
melianamarissa@gmail.com <melianamarissa@gmail.com>
2026-05-31 17:47:43 +0800

    Add preview for image and fix the error for keaving reviews

--- group-24/reviews/get_reviews.php
+++ group-24/reviews/get_reviews.php
@@ -1,9 +1,16 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require __DIR__ . "/../db.php";
+
 try {
    $db = DB::connect();
    $item_id = $_GET['item_id'] ?? null;
