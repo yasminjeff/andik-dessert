8448a54978dd57abf1247b3f182d75e0f07b9cc1
melianamarissa@gmail.com <melianamarissa@gmail.com>
2026-05-31 15:19:38 +0800

    Db php fix

--- group-24/db.php
+++ group-24/db.php
@@ -1,14 +1,15 @@
 <?php
 class DB {
     public static function connect() {
-        if ($_SERVER['HTTP_HOST'] === 'localhost') {
+        if ($_SERVER['HTTP_HOST'] === 'localhost' || str_starts_with($_SERVER['HTTP_HOST'], 'localhost:')) {
             $db = new PDO('sqlite:' . __DIR__ . '/Database1.db');
         } else {
-            $host = 'sql306.infinityfree.com';
-            $dbname = 'if0_41989340_andiks_desserts';
-            $username = 'if0_41989340';
-            $password = 'jgwbK3TOo7xn';
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+            $host = 'zephyr.proxy.rlwy.net';
+            $dbname = 'railway';
+            $username = 'root';
+            $password = 'OdiCtRykjBAYZuuOQDmppeALkvipdYaO';
+            $port = '45879';
+            $db = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
         }
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
