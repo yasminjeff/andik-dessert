862d534a5549c1c3da2056ab676cae457c63647c
melianamarissa@gmail.com <melianamarissa@gmail.com>
2026-06-04 10:20:43 +0800

    Put 3 environment to connect with database and fix the path in login,html after found out not found during log in in localhost

--- group-24/db.php
+++ group-24/db.php
@@ -2,15 +2,17 @@
 class DB {
     public static function connect() {
         if ($_SERVER['HTTP_HOST'] === 'localhost' || str_starts_with($_SERVER['HTTP_HOST'], 'localhost:')) {
-            $db = new PDO('sqlite:' . __DIR__ . '/Database1.db');
-        } else {
-            $host = 'zephyr.proxy.rlwy.net';
-            $dbname = 'railway';
-            $username = 'root';
-            $password = 'OdiCtRykjBAYZuuOQDmppeALkvipdYaO';
-            $port = '45879';
-            $db = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
-        }
+    $db = new PDO("mysql:host=localhost;dbname=andiks_desserts;charset=utf8mb4", "root", "");
+} elseif (str_contains($_SERVER['HTTP_HOST'], 'infinityfree') || str_contains($_SERVER['HTTP_HOST'], 'epizy') || str_contains($_SERVER['HTTP_HOST'], 'infinityfreeapp')) {
+    $db = new PDO('mysql:host=sql306.infinityfree.com;dbname=if0_41989340_andiks_desserts;charset=utf8mb4', 'if0_41989340', 'jgwbK3TOo7xn');
+} else {
+    $host = 'zephyr.proxy.rlwy.net';
+    $dbname = 'railway';
+    $username = 'root';
+    $password = 'OdiCtRykjBAYZuuOQDmppeALkvipdYaO';
+    $port = '45879';
+    $db = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
+}
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     }
